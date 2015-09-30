4d9a839c96827cc7f316159c68a65732b6a368f0
Trevor Salom <trvrsalom@gmail.com>
2015-09-29 23:12:37 -0700

    Templates are now recreated for scouting from the databse, and can succesfully get the data from the templates.

--- clients/web/js/controllers/TemplateController.js
+++ clients/web/js/controllers/TemplateController.js
@@ -2,5 +2,39 @@ app.controller('TemplateController', ['$scope', 'templates', '$routeParams', fun
   templates.success(function(data) {
     data = data["data"];
     $scope.template = data[$routeParams.id];
+
+    for(var i = 10; i < 10000; i *= 10)
+      setTimeout(function() {
+        $('.ui.rating').rating();
+      }, 10);
+
+    $scope.increment = function(item){
+      item.value += item.increment;
+    }
+
+    $scope.decrement = function(item){
+      item.value -= item.increment;
+    }
+
+    $scope.read = function() {
+      console.log(data);
+      for(var i = 0; i < $scope.template.components.length; i++) {
+        var curr = $scope.template.components[i];
+        switch(curr.type) {
+          case 0:
+            console.log(curr.name + " " + $("#" + curr.$$hashKey.replace(":", "\\:")).val());
+            break;
+          case 1:
+            console.log(curr.name + " " + curr.value);
+            break;
+          case 2:
+            console.log(curr.name + " " + $("#" + curr.$$hashKey.replace(":", "\\:")).val());
+            break;
+          case 3:
+            console.log(curr.name + " " + $("#" + curr.$$hashKey.replace(":", "\\:")).rating('get rating'))
+        }
+      }
+    }
+
   });
 }]);
